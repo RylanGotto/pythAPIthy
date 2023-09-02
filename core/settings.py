a24e873f97829a7b439451c106e14b149efc5ff4
Rylan Gotto <rgotto2@gmail.com>
2023-09-02 10:57:18 -0600

    PAPI-4 Adding cors headers
    added cors header middles ware to settings.py

--- core/settings.py
+++ core/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'blog',
     'blog_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +132,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
